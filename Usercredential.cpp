a95b47bee0abad7d7eca02636bc0e5304ab746ce
rajarshi vaibhav <rajarshi vaibhav>
2016-10-04 07:47:14 +0530

    first upload

--- Usercredential.cpp
+++ Usercredential.cpp
@@ -1,8 +1,6 @@
 #include<encryption.cpp>
 
 void Usercredential(string username,string password){
-    username = encryption::encyrpt(username);
-    password = encryption::decrypt(password);
     fstream f;
     f.open("csv files/usercredential.csv",ios::in);
     string fileusername
