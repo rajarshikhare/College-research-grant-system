ee5a81eb645e8c6511ea19d663569697f17d60b2
rajarshi vaibhav <rajarshi vaibhav>
2016-10-05 20:33:59 +0530

    oop

--- Userui.cpp
+++ Userui.cpp
@@ -1,4 +1,5 @@
 #include"Usercredential.cpp"
+#include"encryption.cpp"
 
 void userui(){
     system("cls");
@@ -6,7 +7,9 @@ void userui(){
     string collegename,password;
     cout<<"                                 Enter the college name : ";
     cin>>collegename;
+    collegename = encryption::encrypt(collegename);
     cout<<"                                 Password : ";
     cin>>password;
+    password = encryption::encrypt(password);
     Usercredential(collegename,password);
 }
--- password.cpp
+++ password.cpp
@@ -1,10 +1,23 @@
 #include<iostream>
+#include<conio.h>
 using namespace std;
 
 int main(){
+    int i = 0;
     char c,pass[10];
     while(c != '\n'){
         c = getch();
-        cout<<"*";
+        if(c == '\n'){
+            break;
+        } else if(c == '\b'){
+            cout<<"\b";
+            cout<<" ";
+            cout<<"\b";
+            i--;
+        } else {
+            cout<<"*";
+            pass[i++] = c;
+        }
     }
+    cout<<pass;
 }
