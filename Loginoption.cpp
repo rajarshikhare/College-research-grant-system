b735598196044004fca6c305f0f871a3c0ec888f
rajarshi vaibhav <rajarshi vaibhav>
2016-10-06 08:33:39 +0530

    opp

--- Loginoption.cpp
+++ Loginoption.cpp
@@ -6,9 +6,9 @@ loginoption(){
     cout<<"====================  Ministry of Human Resource Development About MHRD  ========================"<<endl<<endl;
     cout<<"The Ministry of Human Resource Development, formerly Ministry of Education (until 25 September 1985),\nis responsible for the development of human resources in India. The Ministry is divided into two \ndepartments: the Department of School Education and Literacy, which deals with primary, secondary and \nhigher secondary education, adult education and literacy, and the Department of Higher Education, which \ndeals with university education, technical education, scholarship etc."<<endl<<endl<<endl<<endl;
     cout<<"     -----------"<<endl;
-    cout<<"        Login ----------------> Login as"<<endl;
+    cout<<"     *  Login ----------------> Login as"<<endl;
     cout<<"     -----------               ----------------------"<<endl;
-    cout<<"       Register                 1. MHRD Administrator"<<endl;
+    cout<<"     *  Register                1. MHRD Administrator"<<endl;
     cout<<"     -----------               ----------------------"<<endl;
     cout<<"                                2. College/university"<<endl;
     cout<<"                               ----------------------"<<endl<<endl<<endl<<endl<<endl<<endl<<endl;
